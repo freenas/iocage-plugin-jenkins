449faa3968a0a2320f136f93e03f537dd8b8f1de
fulder <misad90@gmail.com>
2021-03-06 13:27:41 +0100

    Add post install echo for initial admin password

--- post_install.sh
+++ post_install.sh
@@ -5,3 +5,6 @@ sysrc -f /etc/rc.conf jenkins_enable="YES"
 
 # Start the service
 service jenkins start 2>/dev/null
+
+# Echo admin password for initial setup
+echo "Initial Admin Password: $(cat /usr/local/jenkins/secrets/initialAdminPassword)"
